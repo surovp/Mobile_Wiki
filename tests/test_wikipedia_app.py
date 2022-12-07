933b8e7ff94f5e8187448d5b61f93012c2bfcbbb
Pavel Surov <surov_p@tanukitech.ru>
2022-12-07 13:21:51 +0500

    added new tests

--- tests/test_wikipedia_app.py
+++ tests/test_wikipedia_app.py
@@ -10,3 +10,35 @@ def test_search():
 
     with allure.step('Check results'):
         results_wiki().should(have.size_greater_than(0))
+
+
+def test_empty_search():
+    with allure.step('Add empty field'):
+        search_wiki().type('')
+
+    with allure.step('Check results'):
+        results_wiki().should(have.size(0))
+
+
+def test_search_numbers():
+    with allure.step('Search numbers'):
+        search_wiki().type('123')
+
+    with allure.step('Check results'):
+        results_wiki().should(have.size_greater_than(0))
+
+
+def test_search_special_symbols():
+    with allure.step('Search symbols'):
+        search_wiki().type('#%')
+
+    with allure.step('Check results'):
+        results_wiki().should(have.size(0))
+
+
+def test_search_html_tag():
+    with allure.step('Search tag'):
+        search_wiki().type('<div>')
+
+    with allure.step('Check results'):
+        results_wiki().should(have.size_greater_than(0))
